1e56801299333cc24b973175f964905a8564e332
Mlie <emipa606@users.noreply.github.com>
2020-03-13 22:40:43 +0100

    First publish

---
+++ Source/CuprosAlloys/PatchOperationFindMod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+using Verse;
+
+namespace CuprosAlloys
+{
+	// Token: 0x02000002 RID: 2
+	internal class PatchOperationFindMod : PatchOperation
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		protected override bool ApplyWorker(XmlDocument xml)
+		{
+			bool flag = this.modName.NullOrEmpty();
+			return !flag && ModsConfig.ActiveModsInLoadOrder.Any((ModMetaData m) => m.Name == this.modName);
+		}
+
+		// Token: 0x04000001 RID: 1
+		private string modName;
+	}
+}
---
+++ Source/CuprosAlloys/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("0.19.0.1")]
+[assembly: AssemblyTitle("CuprosAlloys.Properties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CuprosAlloys.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fffe36c5-bd65-42c5-baa5-b76e76bf359b")]
+[assembly: AssemblyFileVersion("0.19.0.1")]
